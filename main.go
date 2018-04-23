81ccf4691af5842a2a93e59eb02286e6dc9eea9a
Justin Coyne <jcoyne@justincoyne.com>
2018-04-23 11:09:43 -0500

    Extract benchmark to its own package. Add error report

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/sul-dlss-labs/dorilocos/benchmark"
 )
 
 var iterations int
@@ -26,57 +27,16 @@ func init() {
 func main() {
 	flag.Parse()
 	log.Println("Health check")
-	benchmark(iterations, concurrency,
+	benchmark.Run(iterations, concurrency,
 		func(client *http.Client) error {
 			return healthCheckRequest(client)
 		})
 
 	log.Println("Deposit resource")
-	benchmark(iterations, concurrency,
+	benchmark.Run(iterations, concurrency,
 		func(client *http.Client) error {
 			return despositResourceRequest(client)
 		})
-}
-
-func benchmark(n int, concurrency int, method func(*http.Client) error) {
-	results := make(chan time.Duration, concurrency)
-	errors := make(chan error, concurrency)
-
-	requestsPerWorker := n / concurrency
-	log.Printf("Running %v requests on each of %v different workers", requestsPerWorker, concurrency)
-
-	start := time.Now()
-	for w := 1; w <= concurrency; w++ {
-		go worker(requestsPerWorker, results, errors, method)
-	}
-
-	for a := 1; a <= concurrency; a++ {
-		// Wait for each of the workers to report being done.
-		<-results
-	}
-	finish := time.Now()
-	elapsed := finish.Sub(start)
-
-	log.Printf("Total time was %s", elapsed)
-	close(errors)
-	log.Printf("Errors: %v", len(errors))
-	log.Printf("%v requests per second", float64(n)/elapsed.Seconds())
-
-}
-
-func worker(n int, results chan<- time.Duration, errors chan<- error, method func(*http.Client) error) {
-	client := &http.Client{}
-
-	start := time.Now()
-	for i := 0; i < n; i++ {
-		err := method(client)
-		if err != nil {
-			errors <- err
-		}
-	}
-	finish := time.Now()
-	elapsed := finish.Sub(start)
-	results <- elapsed
 }
 
 func healthCheckRequest(client *http.Client) error {
